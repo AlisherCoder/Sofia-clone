c68ab128f0c0c2ea38340427427e2d8c27730d34
Alisher Sharipov <alishersharipov670@gmail.com>
2025-04-27 21:32:48 +0500

    finish

--- js/app.js
+++ js/app.js
@@ -1,9 +1,18 @@
-const headerEl = document.querySelector(".header")
+const headerEl = document.querySelector(".header");
+const backTop = document.querySelector(".back__top");
 
-window.addEventListener("scroll", ()=>{
-    if(document.documentElement.scrollTop > 0){
-        headerEl.classList.add("shrink")
-    }else{
-        headerEl.classList.remove("shrink")
-    }
-})
+window.addEventListener("scroll", () => {
+   let scrollPoint = document.documentElement.scrollTop;
+
+   if (scrollPoint > 0) {
+      headerEl.classList.add("shrink");
+   } else {
+      headerEl.classList.remove("shrink");
+   }
+
+   if (scrollPoint > 200) {
+      backTop.style.transform = "scale(1)";
+   } else {
+      backTop.style.transform = "scale(0)";
+   }
+});
